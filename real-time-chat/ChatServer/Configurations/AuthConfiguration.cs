c86d983e341e0ca53f5a74a90f91768cad49d22e
AdreyAntakov <andrey.antakov05@gmail.com>
2025-09-13 00:04:35 +0300

    AuthConfiguration added

---
+++ real-time-chat/ChatServer/Configurations/AuthConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace ChatServer.Configurations;
+
+public class AuthConfiguration
+{
+    public string Secret { get; set; }
+    public string Issuer { get; set; }
+    public string Audience { get; set; }
+    public TimeSpan Expiration { get; set; }
+}
