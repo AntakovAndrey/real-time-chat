5abea4e196411f067800a7a97fbeb6ac0998758e
AdreyAntakov <andrey.antakov05@gmail.com>
2025-09-12 20:41:17 +0300

    IAppDbContext added

---
+++ real-time-chat/ChatServer/DbContext/IAppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ChatServer.Models;
+using Microsoft.EntityFrameworkCore;
+using File = ChatServer.Models.File;
+
+namespace ChatServer.DbContext;
+
+public interface IAppDbContext
+{
+    public abstract DbSet<User> Users { get; set; }
+    public abstract DbSet<Message> Messages { get; set; }
+    public abstract DbSet<Chat> Chats { get; set; }
+    public abstract DbSet<Session> Sessions { get; set; }
+    public abstract DbSet<File> Files { get; set; }
+  
+    public Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
