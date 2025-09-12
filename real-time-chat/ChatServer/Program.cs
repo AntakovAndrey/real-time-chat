8f37e55876f16701b7761b546a90ffd7e0bcba75
AdreyAntakov <andrey.antakov05@gmail.com>
2025-09-13 00:22:41 +0300

    Fix all issues

--- real-time-chat/ChatServer/Program.cs
+++ real-time-chat/ChatServer/Program.cs
@@ -1,7 +1,31 @@
+using ChatServer.Configurations;
+using ChatServer.DbContext;
 using ChatServer.Hubs;
+using ChatServer.Repositories.Implementations;
+using ChatServer.Repositories.Interfaces;
+using ChatServer.Services.Implementations;
+using ChatServer.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+builder.Services.Configure<AuthConfiguration>(
+    builder.Configuration.GetSection("Auth"));
+builder.Services.AddDbContext<IAppDbContext, AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Repos section
+
+builder.Services.AddTransient<IUserRepository, UserRepository>();
+
+// Services section
+
+builder.Services.AddTransient<IUserService, UserService>();
+
+
+
+builder.Services.AddControllers();
 builder.Services.AddSignalR();
 
 var app = builder.Build();
