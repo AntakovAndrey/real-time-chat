a4a46cd13516cf54ab9bb5aa4f0b181dac1db4ce
AdreyAntakov <andrey.antakov05@gmail.com>
2025-09-12 16:44:55 +0300

    Console chat v0.0.1

---
+++ real-time-chat/chat-console-client/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+var connection = new HubConnectionBuilder()
+    .WithUrl("http://localhost:5000/chat")
+    .WithAutomaticReconnect()
+    .Build();
+
+Console.WriteLine("Connecting...");
+
+connection.On<string>("Receive", (message) =>
+{
+    Console.WriteLine($"Received message: {message}");
+});
+
+try
+{
+    await connection.StartAsync();
+    Console.WriteLine("Connection started");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+
+while (true)
+{
+    var message = Console.ReadLine();
+    await connection.InvokeAsync("Send", message);
+}
--- real-time-chat/chat-server/Hubs/ChatHub.cs
+++ real-time-chat/chat-server/Hubs/ChatHub.cs
@@ -4,9 +4,8 @@ namespace chat_server;
 
 public class ChatHub : Hub
 {
-    public async Task Send(string message, CancellationToken cancellationToken)
+    public async Task Send(string message)
     {
-        await Clients.All.SendAsync("Receive", message, cancellationToken);
+        await Clients.All.SendAsync("Receive", message);
     }
-        
 }
