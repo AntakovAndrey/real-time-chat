89a2f1f39d56eeca5d0c143cf159394746d3d95e
AdreyAntakov <andrey.antakov05@gmail.com>
2025-09-12 12:56:29 +0300

    basic chat server inited

---
+++ real-time-chat/chat-server/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace chat_server;
+
+public class ChatHub : Hub
+{
+    public async Task Send(string message, CancellationToken cancellationToken)
+    {
+        await Clients.All.SendAsync("Receive", message, cancellationToken);
+    }
+        
+}
