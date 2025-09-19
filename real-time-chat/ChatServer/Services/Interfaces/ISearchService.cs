ae185c0c0f9d2e23ebf012318c05c5706e0bb72f
AdreyAntakov <andrey.antakov05@gmail.com>
2025-09-19 23:07:32 +0300

    ISearchService added

--- real-time-chat/ChatServer/Services/Interfaces/ISearchService.cs
+++ real-time-chat/ChatServer/Services/Interfaces/ISearchService.cs
@@ -1,6 +1,11 @@
-﻿namespace ChatServer.Services.Interfaces;
+﻿using ChatServer.Dto;
+
+namespace ChatServer.Services.Interfaces;
 
 public interface ISearchService
 {
-    
+    public Task<QuickSearchResultDto> QuickSearch(string searchTerm, Guid userId, CancellationToken cancellationToken);
+    public Task<List<GetChatDto>> ChatSearch(string searchTerm, Guid userId, CancellationToken cancellationToken);
+    public Task<List<UserDto>> UserSearch(string searchTerm, Guid userId, CancellationToken cancellationToken);
+    public Task<List<GetMessageDto>> MessageSearch(string searchTerm, Guid userId, CancellationToken cancellationToken);
 }
