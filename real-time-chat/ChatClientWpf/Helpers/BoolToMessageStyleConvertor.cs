aac9315e1732735a207db10279d36fca71ce1d93
AdreyAntakov <andrey.antakov05@gmail.com>
2025-10-12 22:33:36 +0300

    Convertors added

---
+++ real-time-chat/ChatClientWpf/Helpers/BoolToMessageStyleConvertor.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ChatClientWpf.Helpers
+{
+    public class BoolToMessageStyleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool isMine)
+            {
+                return isMine ? 
+                    Application.Current.FindResource("MyMessageStyle") : 
+                    Application.Current.FindResource("OtherMessageStyle");
+            }
+            return Application.Current.FindResource("OtherMessageStyle");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ real-time-chat/ChatClientWpf/Helpers/SearchTypeToButtonStyleConverter.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using ChatClientWpf.Enums;
+using ChatClientWpf.Models;
+
+namespace ChatClientWpf.Helpers
+{
+    public class SearchTypeToButtonStyleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is SearchType currentType && parameter is string paramString)
+            {
+                if (Enum.TryParse(paramString, out SearchType paramType))
+                {
+                    return currentType == paramType ? 
+                        Application.Current.FindResource("ActiveSearchTypeButton") : 
+                        Application.Current.FindResource("SearchTypeButton");
+                }
+            }
+            return Application.Current.FindResource("SearchTypeButton");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
