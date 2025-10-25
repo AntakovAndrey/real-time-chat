91117507f05432a40065d439ac3f05595bed35bb
AdreyAntakov <andrey.antakov05@gmail.com>
2025-10-26 00:33:34 +0300

    CreateChatView added

---
+++ real-time-chat/ChatClientWpf/Views/CreateChatView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace ChatClientWpf.Views;
+
+public partial class CreateChatView : UserControl
+{
+    public CreateChatView()
+    {
+        InitializeComponent();
+    }
+}
